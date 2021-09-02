d0a88d4739063bfeb46821a4185adff9761d0416
rmadambat <rajeesh@6on6.com>
2021-09-02 15:03:07 +0530

    Initial checkin

---
+++ DllReplacer/DllReplacer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DllReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtDirectorySearch = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(317, 384);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(208, 70);
+            this.btnReplace.TabIndex = 0;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(407, 107);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(377, 26);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(698, 151);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(86, 32);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Where do you need to replace";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(407, 232);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.ReadOnly = true;
+            this.txtDirectory.Size = new System.Drawing.Size(377, 26);
+            this.txtDirectory.TabIndex = 5;
+            // 
+            // txtDirectorySearch
+            // 
+            this.txtDirectorySearch.Location = new System.Drawing.Point(698, 274);
+            this.txtDirectorySearch.Name = "txtDirectorySearch";
+            this.txtDirectorySearch.Size = new System.Drawing.Size(86, 32);
+            this.txtDirectorySearch.TabIndex = 6;
+            this.txtDirectorySearch.Text = "Search";
+            this.txtDirectorySearch.UseVisualStyleBackColor = true;
+            this.txtDirectorySearch.Click += new System.EventHandler(this.txtDirectorySearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 525);
+            this.Controls.Add(this.txtDirectorySearch);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReplace);
+            this.Name = "Form1";
+            this.Text = "Replace DLL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button txtDirectorySearch;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DllReplacer/DllReplacer/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DllReplacer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.Title = "Browse DLL";
+            openFileDialog1.DefaultExt = "dll";
+            openFileDialog1.Filter = "dll files (*.dll)|*.dll";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Multiselect = false;
+
+            
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtFileName.Text == string.Empty || txtDirectory.Text == string.Empty)
+                {
+                    btnReplace.Enabled = false;
+                    return;
+                }
+
+                FileVersionInfo vinfo = FileVersionInfo.GetVersionInfo(txtFileName.Text);
+                string[] filesToBeRepalced = Directory.GetFiles(txtDirectory.Text, "*.dll", SearchOption.AllDirectories);
+
+                foreach (var item in filesToBeRepalced)
+                {
+                    FileVersionInfo dinfo = FileVersionInfo.GetVersionInfo(item);
+                    if (dinfo.OriginalFilename == vinfo.OriginalFilename && dinfo.FileVersion == vinfo.FileVersion)
+                    {
+                        File.Copy(vinfo.FileName, dinfo.FileName, true);
+
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            // Clear previous select
+            txtFileName.Text = string.Empty;
+            btnReplace.Enabled = false;
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+           if(dr == DialogResult.OK)
+            {
+                txtFileName.Text = openFileDialog1.FileName;
+                btnReplace.Enabled = true;
+            }
+
+        }
+
+        private void txtDirectorySearch_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                txtDirectory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
